552543dde6b080c8940f12c1ff0bcf4369864903
Guess-dls <ninopaket@gmail.com>
2025-10-30 03:21:34 +0000

    Mise à jour du formulaire sendmail et du controller MailTest

--- app/Http/Controllers/MailTestController.php
+++ app/Http/Controllers/MailTestController.php
@@ -4,14 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\SimpleMail;
 
 class MailTestController extends Controller
 {
     // Affiche le formulaire
     public function index()
     {
-        return view('sendmail'); // garde ta vue telle quelle
+        return view('sendmail'); // ta vue reste inchangée
     }
 
     // Envoie le mail
@@ -23,7 +22,18 @@ class MailTestController extends Controller
         ]);
 
         try {
-            Mail::to($request->email)->send(new SimpleMail($request->message));
+            // Envoi du mail directement sans Mailable
+            Mail::send([], [], function ($mail) use ($request) {
+                $mail->to($request->email)
+                    ->subject('Message depuis le formulaire')
+                    ->setBody(
+                        '<p>Vous avez reçu le message suivant :</p>
+                        <p>' . htmlspecialchars($request->message) . '</p>
+                        <p>Merci,<br>' . config('app.name') . '</p>',
+                        'text/html'
+                    );
+            });
+
             return back()->with('success', 'Mail envoyé avec succès !');
         } catch (\Exception $e) {
             return back()->with('error', 'Erreur SMTP : ' . $e->getMessage());
